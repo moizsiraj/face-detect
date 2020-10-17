a2aae20169407ed09d4d02a384e4895e34a29455
Abdul Moiz Siraj <abdulmoiz68@gmail.com>
2020-10-17 17:25:46 +0500

    frontend ready

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Navigation from "./components/navigation/navigation";
+import Logo from "./components/logo/logo";
+import ILF from "./components/ILF/ILF";
+import Rank from "./components/rank/rank";
+import Face from "./components/face/face";
+import Particles from "react-particles-js";
+import Clarifai from "clarifai";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const options = {
+  particles: {
+    number: {
+      value: 150,
+      density: {
+        enabled: true,
+        value_area: 800,
+      },
+    },
+  },
+};
+
+const app = new Clarifai.App({
+  apiKey: "d7b607614ea34644a6a42d2f166d843b",
+});
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      imageUrl: "",
+    };
+  }
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  };
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+    console.log("click");
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+      function (response) {
+        console.log(response);
+      },
+      function (err) {}
+    );
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles" params={options} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ILF
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <Face imageUrl={this.state.imageUrl} />
+      </div>
+    );
+  }
 }
 
 export default App;
